8106ce6c0ce6d8cf8e9b5615549f478f03ca1c6b
Hamoud9876 <h.ghaleb95@gmail.com>
2025-06-07 22:38:27 +0100

    update: removed some random code that was in the file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
-from fastapi import FastAPI
-from db.db_connection import connection
-
-app = FastAPI()
+from db.seed import seed
+import asyncio
 
 
-@app.get("/get_games")
-def get_healthcheck():
-    conn = connection()
-    query = """SELECT * FROM games order by play_time;"""
-    db_respones = conn.run(query)
-    return {
-        "games": [{"game_name": game[2], "play_time": game[3]} for game in db_respones]
-    }
+def main():
+    asyncio.run(seed())
+
+
+main()
