8547a7b911ffc6e5b0ac09c45361322c1cd8c6d8
Hamoud9876 <h.ghaleb95@gmail.com>
2025-06-07 22:39:55 +0100

    update: removed the asynchronous code as it is not suitable for the current solution and replaced it with using request package

--- src/steam_personal_states.py
+++ src/steam_personal_states.py
@@ -1,12 +1,10 @@
+from db.db_connection import connection
+from dotenv import load_dotenv
+from utility.transform_player_data import transform_player_data
 import requests
 import time
 import os
-from db.db_connection import connection
-from dotenv import load_dotenv
-from db.insert_into_tables import *
-from db.db_connection import connection
-import asyncio
-import aiohttp
+import pandas as pd
 
 load_dotenv()
 
@@ -29,50 +27,22 @@ def pull_steam_data():
                 raise Exception
             else:
                 break
-
-    except Exception:
-        return "no information found"
     except TooManyCalles:
         return "calls to the api had failed, try again later"
+    except Exception:
+        return "no information found"
+    
+    game_data_df = transform_player_data({"player_id": player_id, "steam_data": steam_response.json()})
 
-    return {"player_id": player_id, "steam_data": steam_response.json()}
-
-
-async def get_game_genres_categories_price():
-
-    conn = connection()
-    query = """SELECT appid FROM games where appid != 0 ORDER BY appid"""
-    db_response = conn.run(query)
-    appids = [row[0] for row in db_response]
-    await process_http_requests(appids)
-    conn.close()
+    pass
 
 
-async def process_http_requests(appids):
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch_game_data(session, appid) for appid in appids]
-        result = await asyncio.gather(*tasks)
-        insert_into_category_table_bulk(result)
-        insert_into_genres_table_bulk(result)
-        insert_into_prices_table_bulk(result)
-
-
-semaphore = asyncio.Semaphore(1)
-
-
-async def fetch_game_data(session, appid):
-    async with semaphore:
-        await asyncio.sleep(1)
-        url = f"https://store.steampowered.com/api/appdetails?appids={appid}"
-    try:
-        async with session.get(url) as response:
-            if response.status == 200:
-                return appid, await response.json()
-    except Exception as e:
-        print(appid, e)
-        conn = connection()
-        query = "INSERT INTO failed_keys(appid) VALUES (:appid)"
-        conn.run(query, appid=appid)
+def get_game_details(game_data_df: pd.DataFrame):
+    game_details_list = []
+    for index, game in game_data_df.iterrows():
+        game_details_list.append(requests.get(f"https://store.steampowered.com/api/appdetails?appids={game["appid"]}"))
+        
+    
 
 
 class TooManyCalles(Exception):
